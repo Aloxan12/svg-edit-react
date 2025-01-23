9c87f63ccba6068d297618f0b27bba60e21064b6
Aloxan12 <kvakvaaaak@gmail.com>
2025-01-23 11:09:38 +0300

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
     plugins: [react(), svgr()],
+    base: '/svg-edit-react/',
     build: {
         rollupOptions: {
             output: {
